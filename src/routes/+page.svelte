17989c214a08c1e24dd9d003d2d788d2bb60c2d2
theo <lesgooo1993@proton.me>
2024-04-02 15:36:24 +0530

    removed buttons, selected and shit; simple af

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,23 @@
 	let stickers = import.meta.glob("/src/stickers/*.svg", { eager: true });
 	let designs = import.meta.glob("/src/designs/*.svg", { eager: true });
 
-	let selected = stickers
-
-	const btnClass = "text-2xl w-full border-2 border-neutral-500 rounded p-2"
+	const h1class = "my-4 font-bold text-4xl"
 </script>
 
 <Logo />
 
-<div class="w-full flex justify-between mb-4 gap-2">
-	<button class="{btnClass} {selected === stickers ? 'bg-slate-800' : ''}"
-	on:click={() => selected = stickers}>Stickers</button>
-
-	<button class="{btnClass} {selected === designs ? 'bg-slate-800' : ''}"
-	on:click={() => selected = designs}>Designs</button>
-</div>
+<h1 class={h1class}>Stickers</h1>
 
 <div class="grid grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-4 mb-4">
-	{#each Object.keys(selected) as sticker}
+	{#each Object.keys(stickers) as sticker}
 		<img src={sticker} alt="img: {sticker}" class="rounded xl:rounded-lg aspect-square object-cover h-full w-full"/>
+	{/each}
+</div>
+
+<h1 class={h1class}>Designs</h1>
+
+<div class="grid grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-4 mb-4">
+	{#each Object.keys(designs) as design}
+		<img src={design} alt="img: {design}" class="rounded xl:rounded-lg aspect-square object-cover h-full w-full"/>
 	{/each}
 </div>
