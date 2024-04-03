515dfe2ce37a92e3a77de2b37aacb2d3aa0da590
theo <lesgooo1993@proton.me>
2024-04-03 23:55:39 +0530

    moved svgs to static to avoid build error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <script>
-	let stickers = import.meta.glob("/src/stickers/*.svg", { eager: true });
-	let designs = import.meta.glob("/src/designs/*.svg", { eager: true });
+	let stickers = import.meta.glob("/static/stickers/*.svg", { eager: true });
+	let designs = import.meta.glob("/static/designs/*.svg", { eager: true });
 
 	const h1class = "my-4 text-4xl text-blue-400"
+	const svgClass = "rounded xl:rounded-lg aspect-square object-cover h-full w-full"
 </script>
 
 <h1 class={h1class}>സ്റ്റിക്കറുകൾ</h1>
 
 <div class="grid grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-4 mb-4">
 	{#each Object.keys(stickers) as sticker}
-		<img src={sticker} alt="img: {sticker}" class="rounded xl:rounded-lg aspect-square object-cover h-full w-full"/>
+		<img src={sticker.replace("/static", "")}
+		alt="img: {sticker}" class={svgClass}/>
 	{/each}
 </div>
 
@@ -17,6 +19,7 @@
 
 <div class="grid grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-4 mb-4">
 	{#each Object.keys(designs) as design}
-		<img src={design} alt="img: {design}" class="rounded xl:rounded-lg aspect-square object-cover h-full w-full"/>
+		<img src={design.replace("/static", "")}
+		alt="img: {design}" class={svgClass}/>
 	{/each}
 </div>
