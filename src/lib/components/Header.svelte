81a13702db3f929101716fd03e797c2e57f8bca3
theo <lesgooo1993@proton.me>
2024-04-03 23:38:19 +0530

    hid foss-forever.svg; header css fixes

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,10 +9,12 @@
     <img src="/curve.svg" alt="te" class="w-full h-fit"/>
 
 
-    <div class="absolute flex flex-col gap-4 justify-center items-center left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-dvw">
+    <div class="absolute flex h-60 flex-col gap-4 justify-center items-center left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-dvw">
         <Logo/>
 
-        <span class="text-3xl text-blue-400">മലയാളി ഡെവ് സ്റ്റിക്കറുകൾ!</span>
+        <span class="text-3xl text-blue-400 text-center">
+            മലയാളി ഡെവ് സ്റ്റിക്കറുകൾ!
+        </span>
 
         <a class="text-xl bg-gradient-to-b from-blue-500 to-blue-400 text-white shadow rounded py-2 px-8 not-italic" href="#">
             <Github />
