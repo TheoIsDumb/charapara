5aa72490d96069837aa18ffe9867b7ebbb5420f4
theo <lesgooo1993@proton.me>
2024-08-25 13:17:04 +0530

    adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-auto";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
-  },
+import adapter from '@sveltejs/adapter-static';
 
-  preprocess: [vitePreprocess({})],
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
-
-export default config;
