d39272775f4e5e4971e9bcb4be38b6483dfa865c
theo <lesgooo1993@proton.me>
2024-08-25 16:41:52 +0530

    credit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	let stickers = import.meta.glob("/static/stickers/*.svg", { eager: true });
 	let designs = import.meta.glob("/static/designs/*.svg", { eager: true });
 
-	const h1class = "py-4 text-4xl text-blue-400"
+	const h1class = "text-4xl text-blue-400"
 	const svgClass = "rounded xl:rounded-lg aspect-square h-full w-full"
 </script>
 
@@ -18,16 +18,22 @@
 <h1 class={h1class}>സംഭാവന 🗿</h1>
 
 <div class="grid grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-4">
-	<div class="community">
+	<div class="community flex flex-col gap-1">
 		<img src="/python.svg" alt="python" class="{svgClass} hover:-rotate-6 transition duration-300"/>
 
-		<p class="text-center">മെയ്ഡ് ബൈ <a href="https://linkedin.com/in/ajithyesss" class="underline text-blue-300" target="_blank">അജിത്തണ്ണൻ</a></p>
+		<p class="text-center leading-4">
+			മെയ്ഡ് ബൈ
+			<a href="https://linkedin.com/in/ajithyesss" class="underline text-blue-300" target="_blank">അജിത്തണ്ണൻ</a>
+			<br>
+			& ഐഡിയ ബൈ 
+			<a href="https://andrewbast.in/" class="underline text-blue-300" target="_blank">ആൻഡ്രൂ അണ്ണൻ</a>
+		</p>
 	</div>
 </div>
 
 <h1 class={h1class}>വരപ്പ്</h1>
 
-<div class="grid grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-4">
+<div class="grid grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-4 pb-8">
 	{#each Object.keys(designs) as design}
 		<img src={design.replace("/static", "")}
 		alt="img: {design}" class={svgClass}/>
